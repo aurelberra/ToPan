6fab68b4fe7ff2bfd481f63447222a46f07a85b3
Aurélien Berra <aurelien.berra@gmail.com>
2018-03-14 17:43:04 +0100

    update test

---
+++ topan.R
@@ -0,0 +1,11 @@
+# ToPan
+## https://github.com/ThomasK81/ToPan
+
+source("~/Documents/github/r-dev/helpers.R")
+p_load(shiny, LDAvis, XML, xml2, RCurl, httr, lda, servr, markdown, data.table, stringr, plyr, ggplot2, jsonlite, shiny)
+
+# Run demo from GitHub repo
+# runUrl("https://github.com/ThomasK81/ToPan/archive/master.zip")
+
+# Run from local cloned version
+runApp("~/Documents/github/ToPan/")
